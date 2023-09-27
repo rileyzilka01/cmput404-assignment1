639e2d11b7c8f8195afb325c7f7ed40c9742f259
Riley Zilka <rileyzilka33@gmail.com>
2023-09-27 11:30:59 -0600

    progress

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 #  coding: utf-8 
 import socketserver
+import os
 
 # Copyright 2013 Abram Hindle, Eddie Antonio Santos
 # 
@@ -28,11 +29,73 @@ import socketserver
 
 
 class MyWebServer(socketserver.BaseRequestHandler):
-    
+
     def handle(self):
-        self.data = self.request.recv(1024).strip()
-        print ("Got a request of: %s\n" % self.data)
-        self.request.sendall(bytearray("OK",'utf-8'))
+        #self.request is the socket
+        self.data = self.request.recv(1024)
+
+        args = self.data.decode('utf8').split(" ") #Split up the request for headers
+        method = args[0]
+        url = args[1]
+        print(f"Request Method: {method}\nRequested File: {url}") #Debug string for developers
+
+        if method in ["PUT", "POST", "DELETE"]:
+            self.request.sendall("405")
+            return
+
+        if url == '/deep':
+            status_line = "HTTP/1.1 301 Moved Permanently\r\n"
+            location_header = f"Location: {url}/\r\n"
+            response = status_line + location_header
+            self.request.sendall()
+
+        if url != '/': #If the URL is not the homepage
+            self.sendFile(url)
+       
+        print (f"Got a request of: {self.data}\n") #Prints the entire request with headers, if no file requested
+        #self.request.sendall("HTTP/1.1 200 OK\r\n".encode())
+
+    def sendFile(self, url):
+        '''
+        This method will take the url and send the requested file with the header
+        One note is that the response
+        '''
+        url = url.split("?")[0] #Remove the paramters
+        body, code = self.loadFile(url) #Get the body (html content)
+        head = f"HTTP/1.1 {code} OK\r\nContent-Type: text/html\r\n" #Create the response header
+        response = head + body #Join the head and body
+        self.request.send(str(response).encode()) #Send response
+
+    def loadFile(self, url):
+        '''
+        This method will attempt to open the file requested, 
+        If the file exists it will return a 200 code and the file
+        If it doesnt exist it will return a 404 code and the 404 HTML page
+        '''
+        if url in self.compileFiles():
+            with open('www'+url, 'r') as f:
+                response = f.read()
+                f.close()
+            code = 200
+        else:
+            f = open('www/404.html', 'r')
+            response = f.read()
+            f.close()
+            code = 404
+
+        return response, code
+
+    def compileFiles(self):
+        path = './www'
+        file_list = []
+        for root, dirs, files in os.walk(path):
+            for file in files:
+                file_list.append(root+'/'+file)
+
+        for i in range(len(file_list)):
+            file_list[i] = file_list[i][5:]
+            file_list[i] = file_list[i].replace('\\', '/')
+        return file_list
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
